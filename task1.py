f3333b302a7be0a449aa7b12be1645f13b17079a
AshmalFatima <ashmalfatima25@gmail.com>
2026-09-09 10:41:58 +0500

    Add factorial exercises

---
+++ task1.py
@@ -0,0 +1,14 @@
+a = int (input("Enter the number "))
+factorial = 1
+if(a!=0 or a!=1 ):
+    while (a>1):
+        factorial = factorial * a
+        a= a-1
+print ("Factorial:" , factorial)
+
+
+
+
+
+
+
